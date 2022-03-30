13942f071efd710c4822275b242c94b34cf9da65
Anh-Vu Nguyen <59536014+anhvung@users.noreply.github.com>
2022-03-30 15:49:09 -0400

    Update image_extraction.py

--- image_extraction.py
+++ image_extraction.py
@@ -7,7 +7,7 @@ def transform_all(data_path="./e6691-bucket-videos/surgery.videos.hernia/"):
     
     directory = os.fsencode(data_path)
     njobs=2
-    with multiprocessing.Pool(4) as p:
+    with multiprocessing.Pool(16) as p:
         results=p.map(workers.process_vid, os.listdir(directory))
     return results
 transform_all()
